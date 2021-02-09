5b22cedf43042517210b2a95c4182dff3285c3fe
Theo Nguyen <neosndt@gmail.com>
2021-02-08 23:09:43 -0500

    Fix JS bugs
    - JS housekeeping
    - Workaround found for /#projects
    - Change resume link approach

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ const swup = new Swup();
 
 const menuButton = document.querySelector('#hamburger');
 const navBar = document.querySelector('nav');
+const workButton = navBar.querySelector('ul a:nth-of-type(2)');
 const darkButton = navBar.querySelector('div button:first-of-type');
 const motionButton = navBar.querySelector('div button:last-of-type');
-const readButton = document.querySelector('.read-more');
 let oldScroll = 0;
 let expireTime = new Date();
 expireTime.setTime(expireTime.getTime() + 604800*1000);
@@ -31,11 +31,11 @@ darkButton.addEventListener('click', setMode);
 //reduce motion toggle
 motionButton.addEventListener('click', () => {
     if (!document.documentElement.classList.contains('re-mo')){
-        destrVanilla();
         document.documentElement.classList.add('re-mo');
+        destrVanilla();
     } else {
-        initVanilla();
         document.documentElement.classList.remove('re-mo');
+        initVanilla();
     }
 })
 
@@ -78,30 +78,47 @@ VanillaTilt.init(navBar, {
     easing: "cubic-bezier(0.33, 1, 0.68, 1)"
 });
 
+//assign button to scroll content
+scrollButtons = (a, b, c) => {
+    console.log('ran');
+    if (a){
+        console.log('true');
+        a.addEventListener('click', () => {
+            b.scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+            });
+            if (c == 1){
+                toggleMenu();
+            };
+        });
+    }
+}
+//nav contact button
+scrollButtons(navBar.querySelector('ul a:last-of-type'), document.querySelector('footer'), 1);
+
 init = () => {
+    const tilt =  document.querySelectorAll('.tilt');
+    const fullImg = document.querySelectorAll('.full-img');
+    
+    //check if the page address contains projects section the scrolls to top
     if (!window.location.href.includes('#projects')){
         window.scrollTo(0, 0);
     } else {
-        document.querySelector('#projects').scrollIntoView({
-            behaviour: "smooth",
-            block: "start"
-        })
+        document.querySelector('#projects').scrollIntoView();
     }
     
+    //inititalize relax
     if (document.querySelector('.about header > *')){
         var rellax = new Rellax('.about header > *', {
             center: true,
         });
     }
-    
     if (document.querySelector('.home header')){
         var rellax = new Rellax('.home header > *:not(.gradie)', {
             center: true,
         });
     }
-    
-    const tilt =  document.querySelectorAll('.tilt');
-    const fullImg = document.querySelectorAll('.full-img');
 
     //initialize flickity
     if (document.querySelector('.main-carousel')){
@@ -114,21 +131,24 @@ init = () => {
 
     //initialize vanilla tilt
     initVanilla = () => {
-        if (tilt){
-            VanillaTilt.init(tilt, {
-                perspective: 670,
-                easing: "cubic-bezier(0.33, 1, 0.68, 1)"
-            });
-        }
-        if (fullImg){
-            VanillaTilt.init(fullImg, {
-                perspective: 1000,
-                max: 11,
-                easing: "cubic-bezier(0.33, 1, 0.68, 1)"
-            });
+        if (!document.documentElement.classList.contains('re-mo')){
+            if (tilt){
+                VanillaTilt.init(tilt, {
+                    perspective: 800,
+                    easing: "cubic-bezier(0.33, 1, 0.68, 1)"
+                });
+            }
+            if (fullImg){
+                VanillaTilt.init(fullImg, {
+                    perspective: 1000,
+                    max: 11,
+                    easing: "cubic-bezier(0.33, 1, 0.68, 1)"
+                });
+            }
         }
     }
     initVanilla();
+    
     //destroy vanilla tilt
     destrVanilla = () =>{
         if (tilt[0]){tilt.forEach(tlit => {tlit.vanillaTilt.destroy();});}
@@ -147,38 +167,33 @@ init = () => {
             document.body.classList.remove('scroll-lock', 'modal');
         }
     }
-    if (document.querySelectorAll('.full-img')[0]){
-        document.querySelectorAll('.full-img').forEach(lilimg => {
-            lilimg.addEventListener('click', () => {
-                modalCheck(lilimg);
-            })
-        })
+    if (fullImg[0]){
+        fullImg.forEach(lilimg => {lilimg.addEventListener('click', () => {modalCheck(lilimg);})})
     }
     
-    //assign button to scroll content
-    scrollButtons = (a, b, c) => {
-        if (a){
-            a.addEventListener('click', () => {
-                b.scrollIntoView({
-                    behavior: "smooth",
-                    block: "start",
-                })
-                if (c == 1){
-                    toggleMenu();
-                }
-            })
-        }
-    }
-    scrollButtons(readButton, document.querySelector('#process'), 0);
+    //read-more button on project pages
+    scrollButtons(document.querySelector('.read-more'), document.querySelector('#process'), 0);
+    //home nav navigation
     if (document.querySelector('.home')){
-        scrollButtons(navBar.querySelector('ul a:nth-of-type(2)'), document.querySelector('#projects'), 1);
         scrollButtons(document.querySelector('header h3'), document.querySelector('#projects'), 0);
     }
-    scrollButtons(navBar.querySelector('ul a:last-of-type'), document.querySelector('footer'), 1);
+    //about page button
     scrollButtons(document.querySelector('.about header a'), document.querySelector('#contact'), 0);
     
+    //nav work button, since it is not replaced by swup, workarounds are needed
+    if (document.querySelector('.home')){
+        workButton.setAttribute("href", "#projects");
+        workButton.setAttribute("data-no-swup", "");
+        document.documentElement.classList.add('scroll-smooth');
+        workButton.addEventListener('click', toggleMenu);
+    } else {
+        workButton.setAttribute("href", "/#projects");
+        workButton.removeAttribute("data-no-swup");
+        workButton.removeEventListener('click', toggleMenu);
+    }
+    
+    //change mode by what is set in cookie
     keepMode = () => {
-        //change mode by what is set in cookie
         if (getCookie("settings") != undefined) {
             document.documentElement.classList.add(getCookie("settings"));
         } else {
@@ -194,5 +209,6 @@ swup.on('contentReplaced', init);
 unload = () => {
     document.querySelector('nav').classList.remove('menu-active');
     document.body.classList.remove('scroll-lock');
+    document.documentElement.classList.remove('scroll-smooth');
 }
 swup.on('willReplaceContent', unload);
